b48f3b2080a3907af0e57f377583fda4a1633fa9
GroveDG <grovedgdev@gmail.com>
2025-09-24 10:58:03 -0400

    non-closure funcs

--- ure/data/src/func.rs
+++ ure/data/src/func.rs
@@ -1,21 +1,10 @@
-use std::{
-    any::{Any, TypeId},
-    marker::PhantomData,
-};
+use std::any::TypeId;
 
-use crate::data::{ComponentId, Components, Container};
+use crate::data::{ComponentId, Components};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ConstantId {
+pub struct FunctionId {
     inner: u64,
-}
-pub struct Constant<T: Any> {
-    id: ConstantId,
-    _marker: PhantomData<T>,
-}
-
-pub struct Function<T: Any> {
-    inner: Constant<T>,
 }
 
 pub struct Impl<Func> {
@@ -42,52 +31,42 @@ impl<F: Copy> Func<F> {
     }
 }
 
-const A: ComponentId = ComponentId::new("example");
-const B: ComponentId = ComponentId::new("example");
-
-// const EXAMPLE: Func<fn(&mut dyn Container, &dyn Container, bool) -> f32> = Func {
-//     components: &[A, B],
-//     impls: &[Impl {
-//         component_types: &[TypeId::of::<Vec<usize>>, TypeId::of::<Vec<usize>>],
-//         implementation: |a, b, c| {
-//             (|a: &mut Vec<usize>, b: &Vec<usize>, c: bool| -> f32 { 0.0 })(
-//                 a.downcast_mut().unwrap(),
-//                 b.downcast_ref().unwrap(),
-//                 c,
-//             )
-//         },
-//     }],
-// };
-
 #[macro_export]
 macro_rules! func {
     (
         $func_vis:vis $func_name:ident :
         ($($f_ty:ty $(: $comp_id:expr)?),* $(,)?) $(-> $fr_ty:ty)? =
         $(
-            |$($arg_name:ident $(as mut $comp_mut_ty:ty)? $(as ref $comp_ref_ty:ty)? $(: $arg_ty:ty)?),* $(,)?| $i:block;
-        )*
+            ( $( _ $(as &mut $mut_ty:ty)? $(as &$ref_ty:ty)? ),* $(,)? ) $i:expr
+        ),* $(,)?
     ) => {
 $func_vis const $func_name: $crate::func::Func<fn ($($f_ty),*) $(-> $fr_ty)?> = $crate::func::Func {
     components: &[$($($comp_id,)?)*],
-    impls: &[$(
-        $crate::func!(IMPL |$($arg_name $(as mut $comp_mut_ty)? $(as ref $comp_ref_ty)? $(: $arg_ty)?),*| $i)
-    ),*]
+    impls: &[$($crate::mident::mident!(
+        $crate::func!(IMPL ( $(#rand $(as &mut $mut_ty)? $(as &$ref_ty)?),* ) $i)
+    )),*]
 };
     };
-    (IMPL |$($arg_name:ident $(as mut $comp_mut_ty:ty)? $(as ref $comp_ref_ty:ty)? $(: $arg_ty:ty)?),* $(,)?| $i:block) => {
-Impl {
-    component_types: &[$($(std::any::TypeId::of::<$comp_mut_ty>,)? $(std::any::TypeId::of::<$comp_ref_ty>,)?)*],
-    implementation: |$($arg_name),*| ((|$($arg_name),*| $i) as fn($($(&mut $comp_mut_ty)? $(&$comp_ref_ty)? $($arg_ty)?),*) -> _)($(
-        $arg_name $(.downcast_mut::<$comp_mut_ty>().unwrap())? $(.downcast_ref::<$comp_ref_ty>().unwrap())?
+    (IMPL ( $( $arg_name:ident $(as &mut $mut_ty:ty)? $(as &$ref_ty:ty)? ),* $(,)? ) $i:expr) => {
+$crate::func::Impl {
+    component_types: &[$($(std::any::TypeId::of::<$mut_ty>,)? $(std::any::TypeId::of::<$ref_ty>,)?)*],
+    implementation: |$($arg_name),*| ($i)($(
+        $arg_name $(.downcast_mut::<$mut_ty>().unwrap())? $(.downcast_ref::<$ref_ty>().unwrap())?
     ),*),
 }
     };
 }
 
-func! {
-    pub EXAMPLE: (&mut dyn Container: A, bool) =
-    |a as mut Vec<usize>, b: bool| {example(a, b)};
-}
+// mod example {
+//     use crate::data::{ComponentId, Container};
 
-fn example(a: &mut Vec<usize>, b: bool) {}
+//     const A: ComponentId = ComponentId::new("example_indices");
+
+//     func! {
+//         pub EXAMPLE: (&mut dyn Container: A, bool) =
+//         (_ as &mut Vec<usize>, _) example_vec,
+//     }
+
+//     fn example_vec(a: &mut Vec<usize>, b: bool) {}
+// }
+// pub use example::EXAMPLE;
